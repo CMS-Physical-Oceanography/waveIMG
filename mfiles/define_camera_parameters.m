b135f4dcff3466f2a0b72665d90244c4db271b9e
grimesdj <grimesdj@uncw.edu>
2023-07-03 11:45:21 -0400

    info and camera parameters script

---
+++ mfiles/define_camera_parameters.m
@@ -0,0 +1,18 @@
+% subdirectories
+
+% video info
+info.format = 'avi';
+info.freq   = 2.5;
+% grid info
+info.X_res  = 0.2;
+% other info
+info.Al_bar = 10;
+
+
+
+% camera intrinsic parameters
+icp.PixelFormat = 'RGB';
+
+
+% extrinsic camera parameters
+beta0 = [35.0911  585.0288   42.4419   72.1536    0.1974   44.8175];
